e81bdc068b1851b42cd75e33e29778d0fc4fc1f0
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-29 23:27:55 +0800

    added navbar from mantine ui

---
+++ app/_components/nav/navbar.tsx
@@ -0,0 +1,103 @@
+"use client";
+import cx from "clsx";
+import { useState } from "react";
+import {
+  Container,
+  Avatar,
+  UnstyledButton,
+  Group,
+  Text,
+  Menu,
+  Tabs,
+  Burger,
+  rem,
+  useMantineTheme,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { MantineLogo } from "@mantinex/mantine-logo";
+import classes from "@/components/nav/navbar.module.css";
+
+const user = {
+  name: "Jane Spoonfighter",
+  email: "janspoon@fighter.dev",
+  image:
+    "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png",
+};
+
+const tabs = [
+  "Home",
+  "Orders",
+  "Education",
+  "Community",
+  "Forums",
+  "Support",
+  "Account",
+  "Helpdesk",
+];
+
+export default function Navbar() {
+  const theme = useMantineTheme();
+  const [opened, { toggle }] = useDisclosure(false);
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+
+  const items = tabs.map((tab) => (
+    <Tabs.Tab value={tab} key={tab}>
+      {tab}
+    </Tabs.Tab>
+  ));
+
+  return (
+    <div className={classes.header}>
+      <Container className={classes.mainSection} size="md">
+        <Group justify="space-between">
+          {/* <MantineLogo size={28} /> */}
+
+          <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" />
+
+          <Menu
+            width={260}
+            position="bottom-end"
+            transitionProps={{ transition: "pop-top-right" }}
+            onClose={() => setUserMenuOpened(false)}
+            onOpen={() => setUserMenuOpened(true)}
+            withinPortal
+          >
+            <Menu.Target>
+              <UnstyledButton
+                className={cx(classes.user, {
+                  [classes.userActive]: userMenuOpened,
+                })}
+              >
+                <Group gap={7}>
+                  <Avatar
+                    src={user.image}
+                    alt={user.name}
+                    radius="xl"
+                    size={20}
+                  />
+                  <Text fw={500} size="sm" lh={1} mr={3}>
+                    {user.name}
+                  </Text>
+                </Group>
+              </UnstyledButton>
+            </Menu.Target>
+          </Menu>
+        </Group>
+      </Container>
+      <Container size="md">
+        <Tabs
+          defaultValue="Home"
+          variant="outline"
+          visibleFrom="sm"
+          classNames={{
+            root: classes.tabs,
+            list: classes.tabsList,
+            tab: classes.tab,
+          }}
+        >
+          <Tabs.List>{items}</Tabs.List>
+        </Tabs>
+      </Container>
+    </div>
+  );
+}
