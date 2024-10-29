f7a8bbae0f98d8206bdd56018abf6535ee5ab4ea
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-29 23:26:18 +0800

    changed to auto

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <ColorSchemeScript />
+        <ColorSchemeScript defaultColorScheme="auto" />
         <link rel="shortcut icon" href="/favicon.svg" />
         <meta
           name="viewport"
