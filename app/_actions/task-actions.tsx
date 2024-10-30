8f1329fd76cf5c54354de53330fcfa92a28d5a9e
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-31 00:46:55 +0800

    add accessToken requirement for auth

--- app/_actions/task-actions.tsx
+++ app/_actions/task-actions.tsx
@@ -1,6 +1,13 @@
 "use server";
 import { Task } from "@/types/types";
 
+const ACCESS_TOKEN = process.env.API_ACCESS_TOKEN;
+
+const headers = {
+  "Content-Type": "application/json",
+  Authorization: `Bearer ${ACCESS_TOKEN}`,
+};
+
 // This is how I would filter
 // if the api did not have it built in
 // export default async function getTasks() {
@@ -18,6 +25,7 @@ import { Task } from "@/types/types";
 export async function getTasks(): Promise<Task[]> {
   const response = await fetch(
     "https://jsonplaceholder.typicode.com/todos?userId=1",
+    { headers },
   );
 
   const json = await response.json();
@@ -29,7 +37,7 @@ export async function getTasks(): Promise<Task[]> {
 export async function createTask(title: string): Promise<Task> {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
+    headers,
     body: JSON.stringify({
       title,
       completed: false,
@@ -50,7 +58,7 @@ export async function toggleTask(
     `https://jsonplaceholder.typicode.com/todos/${id}`,
     {
       method: "PATCH",
-      headers: { "Content-Type": "application/json" },
+      headers,
       body: JSON.stringify({ completed }),
     },
   );
@@ -62,5 +70,6 @@ export async function toggleTask(
 export async function deleteTask(id: number): Promise<void> {
   await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
     method: "DELETE",
+    headers,
   });
 }
