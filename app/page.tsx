647e409fbdc5ed5fc345a57b969fcefbb0b9e500
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-31 00:21:43 +0800

    new homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import { Container, Title, Text, Stack } from "@mantine/core";
+
 export default function HomePage() {
-  return <div>Home page</div>;
+  return (
+    <Container size="sm" py={80}>
+      <Stack align="center">
+        <Title order={1}>Task Management App</Title>
+        <Text size="lg">
+          Please log in to access your personal task management dashboard.
+        </Text>
+      </Stack>
+    </Container>
+  );
 }
