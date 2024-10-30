d98da6bf07dc9b3cc019ada13c3b48db6fb2466b
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-30 13:28:12 +0800

    update comments

--- app/_actions/task-actions.tsx
+++ app/_actions/task-actions.tsx
@@ -1,11 +1,24 @@
 "use server";
 
+// This is how I would filter
+// if the api did not have it built in
+// export default async function getTasks() {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+//
+//   const json = await response.json();
+//
+//   const userTasks = json.filter((task: any) => task.userId === 1);
+//
+//   return userTasks;
+// }
+
+// filter in url params
 export default async function getTasks() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos?userId=1",
+  );
 
   const json = await response.json();
 
-  const userTasks = json.filter((task: any) => task.userId === 1);
-
-  return userTasks;
+  return json;
 }
