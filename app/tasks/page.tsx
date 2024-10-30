0009eace86c9b8eb63cfc0c690199ff52e2e55ca
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-30 21:13:55 +0800

    wip now using tasklist component

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,13 +1,12 @@
 import getTasks from "@/actions/task-actions";
+import TaskList from "@/components/task-management/task-list";
 import React from "react";
 
 async function Tasks() {
   const tasks = await getTasks();
   return (
     <div>
-      {tasks.map((task: any) => (
-        <div key={task.id}>{task.title}</div>
-      ))}
+      <TaskList tasks={tasks} />
     </div>
   );
 }
