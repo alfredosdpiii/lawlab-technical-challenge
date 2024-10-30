65632755ce8fabca071d00287dc4d14c59cd08a3
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-30 11:18:00 +0800

    wip can now get tasks

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,7 +1,15 @@
+import getTasks from "@/actions/task-actions";
 import React from "react";
 
-function Tasks() {
-  return <div>TaskManagement</div>;
+async function Tasks() {
+  const tasks = await getTasks();
+  return (
+    <div>
+      {tasks.map((task: any) => (
+        <div key={task.id}>{task.title}</div>
+      ))}
+    </div>
+  );
 }
 
 export default Tasks;
